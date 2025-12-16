533fb1e66f8fe513b010f4040eb8d335ca8fac2c
Nur Farhayati <farhayati.nur@gmail.com>
2025-12-16 21:20:22 +0700

    WIP: local frontend changes

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
 Route::get('/', function () {
-    return Inertia::render('welcome', [
+    return Inertia::render('Welcome', [
         'canRegister' => Features::enabled(Features::registration()),
     ]);
 })->name('home');
